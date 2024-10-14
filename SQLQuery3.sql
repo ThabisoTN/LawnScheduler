1a1ef104d1a8cd7cfcd9d6e901d1f7226179a805
Thabiso Ngubane <90799063+ThabisoTN@users.noreply.github.com>
2024-10-14 12:39:39 +0200

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE Machines (
-    MachineId INT PRIMARY KEY IDENTITY(1,1),
-    MachineName NVARCHAR(100) NOT NULL,
-    Model NVARCHAR(100) NOT NULL,
-    Description NVARCHAR(255) NULL,
-    OperatorId NVARCHAR(450) NOT NULL, -- Foreign key referencing AspNetUsers
-
-    CONSTRAINT FK_Machines_Operator FOREIGN KEY (OperatorId) REFERENCES AspNetUsers(Id) ON DELETE CASCADE
-);
